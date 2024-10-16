5c1914aa3f00ee060325c937b8ac611b6fdbaf58
akankshyam07 <akankshya.m07@gmail.com>
2024-10-16 12:58:46 +0530

    Making it Public
    Making it Public

---
+++ InsertionSort/MyInsertionSort.py
@@ -0,0 +1,16 @@
+"""
+Author: Akankshya Mohanty
+"""
+def insertionSort(list):
+    for i in range(1, len(list), 1):
+        for j in range (i, 0, -1):
+            if list[j] < list [j-1]:
+                temp = list[j]
+                list[j] = list[j-1]
+                list[j-1] = temp
+
+list = [78, 38, 1, 87, 23]
+insertionSort(list)
+
+for item in list:
+    print(item)
